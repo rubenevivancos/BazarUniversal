4f9faf4148af184b2efa9e35fa64fe1f9f389016
rubenevivancos <rubenevivancos@hotmail.com>
2023-11-25 21:21:24 -0500

    Recibiendo el resultado de la busqueda de productos

--- client/src/Redux/Actions/productAction.js
+++ client/src/Redux/Actions/productAction.js
@@ -1,11 +1,13 @@
+import axios from "axios";
+
 import { getByProductReducer } from "../Reducer/productReducer";
 
 export const getByProduct = (product) => async (dispatch) => {
     try {
         console.log("Se busca: " + product);
-        //var response = (await axios.get(`http://localhost:3001/dogs/getByName?name=${name}`)).data;
-        //console.log("[ src/Store/Actions/index.js/getByName(name) ] La busqueda por nombre de: " + name + " encontro " + response.length + " resultados"); 
-        dispatch(getByProductReducer([]));
+        var response = (await axios.get(`http://localhost:3001/dogs/getByName?name=${product}`)).data;
+        console.log("[ getByName(product) ] La busqueda por nombre de: " + product + " encontro " + response.length + " resultados"); 
+        dispatch(getByProductReducer(response));
 
     } catch (error) {
         console.log("[ getByProduct ] Excepcion: error.message: " + error.message);

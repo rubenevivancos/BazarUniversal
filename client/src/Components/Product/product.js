0d73084d570961879f5ec4bb30612fd026a71199
rubenevivancos <rubenevivancos@hotmail.com>
2023-12-06 00:08:02 -0500

    Agregando informacion en cada producto del resultado

--- client/src/Components/Product/product.js
+++ client/src/Components/Product/product.js
@@ -3,7 +3,19 @@ export default function Product({product}) {
     return(
         <div >
             <div>
-                <img src={product.images[0]} alt={dog.name}/>
+                <img src={product.images[0]} alt={product.title}/>
+            </div>
+            <div>
+                {product.title}
+            </div>
+            <div>
+                {product.description}
+            </div>
+            <div>
+                {product.price}
+            </div>
+            <div>
+                {product.rating}
             </div>
         </div>
     )

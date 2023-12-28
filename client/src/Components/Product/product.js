58af909bdc9bf8b5fdcf1a9ecac8d2c00732c706
rubenevivancos <rubenevivancos@hotmail.com>
2023-12-27 20:43:52 -0500

    Mejoras al componente Product

--- client/src/Components/Product/product.js
+++ client/src/Components/Product/product.js
@@ -1,21 +1,23 @@
 export default function Product({product}) {
 
     return(
-        <div >
-            <div>
+        <div class="product-container">
+            <div class="product-image">
                 <img src={product.images[0]} alt={product.title} width="150" height="150"/>
             </div>
-            <div>
-                {product.title}
-            </div>
-            <div>
-                {product.description}
-            </div>
-            <div>
-                {product.price}
-            </div>
-            <div>
-                {product.rating}
+            <div class="product-details">
+                <div class="product-title">{product.title}</div>
+                <div class="product-description">{product.description}</div>
+                <div class="product-price-and-rating">
+                    <div class="product-price">{product.price}</div>
+                    <div class="product-rating">{product.rating}
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                        <span class="star">&#9733;</span>
+                    </div>
+                </div>
             </div>
         </div>
     )

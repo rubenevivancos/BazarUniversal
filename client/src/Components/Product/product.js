206a5d0acc899b917063de92fec1225d09f02055
rubenevivancos <rubenevivancos@hotmail.com>
2023-12-27 20:49:15 -0500

    Archivo estilos del componente Product

--- client/src/Components/Product/product.js
+++ client/src/Components/Product/product.js
@@ -1,21 +1,23 @@
+import "./product.css";
+
 export default function Product({product}) {
 
     return(
-        <div class="product-container">
-            <div class="product-image">
+        <div className="product-container">
+            <div className="product-image">
                 <img src={product.images[0]} alt={product.title} width="150" height="150"/>
             </div>
-            <div class="product-details">
-                <div class="product-title">{product.title}</div>
-                <div class="product-description">{product.description}</div>
-                <div class="product-price-and-rating">
-                    <div class="product-price">{product.price}</div>
-                    <div class="product-rating">{product.rating}
-                        <span class="star">&#9733;</span>
-                        <span class="star">&#9733;</span>
-                        <span class="star">&#9733;</span>
-                        <span class="star">&#9733;</span>
-                        <span class="star">&#9733;</span>
+            <div className="product-details">
+                <div className="product-title">{product.title}</div>
+                <div className="product-description">{product.description}</div>
+                <div className="product-price-and-rating">
+                    <div className="product-price">{product.price}</div>
+                    <div className="product-rating">{product.rating}
+                        <span className="star">&#9733;</span>
+                        <span className="star">&#9733;</span>
+                        <span className="star">&#9733;</span>
+                        <span className="star">&#9733;</span>
+                        <span className="star">&#9733;</span>
                     </div>
                 </div>
             </div>

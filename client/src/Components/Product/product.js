3b6729f7e0297c18ece991e4fc68e13903e87440
rubenevivancos <rubenevivancos@hotmail.com>
2023-12-29 02:30:37 -0500

    Correcciones al estilo del componente Product

--- client/src/Components/Product/product.js
+++ client/src/Components/Product/product.js
@@ -4,22 +4,30 @@ export default function Product({product}) {
 
     // Función para renderizar las estrellas según la calificación
     const renderStars = () => {
-        const stars = [];
-        const integerRating = Math.floor(product.rating); // Parte entera de la calificación
-        const decimalPart = product.rating % 1; // Parte decimal de la calificación
+        let stars = [];
+
+        let integerRating = Math.floor(product.rating); // Parte entera de la calificación
+
+        // Obtenemos la parte decimal restando la parte entera al número original
+        let decimal = product.rating - integerRating;
+        // Multiplicamos por 100 para obtener un número entero (eliminando el punto decimal)
+        let decimalPart = decimal * 100;
+        decimalPart = Math.floor(decimalPart); //Obtenemos la parte entera
+
 
         for (let i = 0; i < 5; i++) {
             if (i < integerRating) {
-                stars.push(<span key={i} className="star active">&#9733;</span>);
-            } else if (i === integerRating && decimalPart > 0) {
-                const decimalWidth = `${decimalPart * 100}%`;
+                stars.push(<span key={i}><span key={i} className="star-active">&#9733;</span><br key={i+1}/><br key={i+2}/></span>);
+            } else {
+                let decimalWidth = `${decimalPart}%`;
                 stars.push(
-                    <span key={i} className="star active" style={{ width: decimalWidth }}>
-                        &#9733;
+                    <span key={i+1}>
+                        <span key={i} className="star-active" style={{ width: decimalWidth }}>
+                            &#9733;
+                        </span><br key={i+1}/><br key={i+2}/>
                     </span>
                 );
-            } else {
-                stars.push(<span key={i} className="star">&#9733;</span>);
+                break;
             }
         }
         return stars;
